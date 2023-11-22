6dc54ede451be57b9826f33743a42a7241c1fe56
Lucas-Dia-99 <lucas.t.dia@gmail.com>
2023-11-22 12:27:14 -0500

    Linegraph
    added linegraph + fixed the size of the treemap

--- js/visualization.js
+++ js/visualization.js
@@ -1,7 +1,6 @@
 // Immediately Invoked Function Expression to limit access to our 
 // variables and prevent 
 ((() => {
-
   d3.csv("data/Federal_Expenditures_Dataset.csv", (fedExpData) => {
     d3.csv("data/Historical_Events.csv", (eventData) => {
       d3.csv("data/Historical_Political_Power.csv", (polPowData) => {
@@ -10,6 +9,12 @@
         const dispatchString = "selectionUpdated";
 
         let selectedYear = "2021"  // Initial year to display
+
+         function createLineGraph(data) {
+          let lgDepExp = linegraph()
+            ('#linegraph', data);
+        }
+        createLineGraph(fedExpData); // Call the function to create the line graph with fedExpData
         let tmDepExp = treemap()
         .value(d => +d["Gross Cost (in Billions)"])
         .label(d => d["Agency Name"])
